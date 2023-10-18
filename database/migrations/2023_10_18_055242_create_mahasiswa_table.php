bbf93c9ed7de4437a4648a12fe25811175b2022b
ChristoferEvan <evan200421@gmail.com>
2023-10-18 13:21:14 +0700

    revisi tabel

--- database/migrations/2023_10_18_055242_create_mahasiswa_table.php
+++ database/migrations/2023_10_18_055242_create_mahasiswa_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('tempat_lahir');
             $table->date('tanggal_lahir');
-
+            $table->timestamps();
             
         });
     }
